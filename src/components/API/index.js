9b5b705a175837f7b9e385ff88fc25b4ee93c4e9
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2023-01-21 18:20:24 +0200

    added language of ukraine

--- src/components/API/index.js
+++ src/components/API/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export function loadDataCurrentWeatherForcast() {
   return axios
     .get(
-      `https://api.openweathermap.org/data/2.5/weather?q=Kyiv,ua&appid=363299d73e8f928e961676fcc902625c`
+      `https://api.openweathermap.org/data/2.5/weather?q=Kyiv,ua&appid=363299d73e8f928e961676fcc902625c&lang=ua`
     )
     .then(({ data }) => {
       return data;
